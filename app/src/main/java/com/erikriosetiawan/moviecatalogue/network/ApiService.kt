2ef408c83703af40db2ceb972f91a1a5e034fd35
erikrios <erikriosetiawan49@gmail.com>
2020-01-03 19:06:39 +0700

    Add method to get tv shows data

--- app/src/main/java/com/erikriosetiawan/moviecatalogue/network/ApiService.kt
+++ app/src/main/java/com/erikriosetiawan/moviecatalogue/network/ApiService.kt
@@ -2,6 +2,7 @@ package com.erikriosetiawan.moviecatalogue.network
 
 import com.erikriosetiawan.moviecatalogue.BuildConfig
 import com.erikriosetiawan.moviecatalogue.models.MovieResponse
+import com.erikriosetiawan.moviecatalogue.models.TvShowResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
@@ -9,4 +10,7 @@ interface ApiService {
 
     @GET("3/discover/movie?api_key=${BuildConfig.API_KEY}&language=en-US")
     fun getMovies(): Call<MovieResponse>
+
+    @GET("3/discover/tv?api_key=${BuildConfig.API_KEY}&language=en-US")
+    fun getTvShows(): Call<TvShowResponse>
 }

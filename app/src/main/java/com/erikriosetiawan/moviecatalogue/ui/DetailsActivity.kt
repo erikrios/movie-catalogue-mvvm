af600ec1fd3e47b21bba0616b5881615db22e235
erikrios <erikriosetiawan49@gmail.com>
2020-01-03 20:39:13 +0700

    Bind layout with data intent

--- app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/DetailsActivity.kt
+++ app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/DetailsActivity.kt
@@ -1,19 +1,19 @@
 package com.erikriosetiawan.moviecatalogue.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.moviecatalogue.R
 import com.erikriosetiawan.moviecatalogue.databinding.ActivityDetailsBinding
 import com.erikriosetiawan.moviecatalogue.models.Movie
 import com.erikriosetiawan.moviecatalogue.models.TvShow
+import com.squareup.picasso.Picasso
 
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
     private lateinit var movie: Movie
     private lateinit var tvShow: TvShow
-    private lateinit var dataKey: String
 
     companion object {
         const val DATA_KEY = "data_key"
@@ -24,7 +24,27 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_details)
+        getDataIntent()
     }
 
+    private fun getDataKey(): String? = intent.getStringExtra(DATA_KEY)
 
+    private fun getDataIntent() {
+        if (getDataKey() == MOVIE_KEY) {
+            movie = intent.getParcelableExtra(MOVIE_KEY) as Movie
+            Picasso.get().load("https://image.tmdb.org/t/p/w185${movie.posterPath}")
+                .into(binding.poster)
+            binding.title.text = movie.title
+            binding.firstAirDate.text = movie.releaseDate
+            binding.overview.text = movie.overview
+
+        } else {
+            tvShow = intent.getParcelableExtra(TV_SHOW_KEY) as TvShow
+            Picasso.get().load("https://image.tmdb.org/t/p/w185${tvShow.posterPath}")
+                .into(binding.poster)
+            binding.title.text = tvShow.title
+            binding.firstAirDate.text = tvShow.firstAirDate
+            binding.overview.text = tvShow.overview
+        }
+    }
 }

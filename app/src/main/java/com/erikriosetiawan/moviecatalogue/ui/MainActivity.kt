1d7293891b70db63dbbf6ebbdbea33a3e8848233
erikrios <erikriosetiawan49@gmail.com>
2020-01-03 08:33:07 +0700

    Configure data binding on main activity

--- app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.erikriosetiawan.moviecatalogue.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.moviecatalogue.R
+import com.erikriosetiawan.moviecatalogue.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }

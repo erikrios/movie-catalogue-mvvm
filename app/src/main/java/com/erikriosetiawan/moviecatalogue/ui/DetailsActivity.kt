498312a843102b8f059784b5e89a4cf8622d263e
erikrios <erikriosetiawan49@gmail.com>
2020-01-03 20:09:38 +0700

    Add details activity

---
+++ app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.erikriosetiawan.moviecatalogue.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erikriosetiawan.moviecatalogue.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}

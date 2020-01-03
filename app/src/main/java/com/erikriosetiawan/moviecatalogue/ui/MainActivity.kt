12f8f2ac84b86a31c0b05518985f24ddad4dda9f
erikrios <erikriosetiawan49@gmail.com>
2020-01-03 08:50:58 +0700

    Add fragment in main activity and set the bottom navigation view

--- app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/MainActivity.kt
@@ -3,14 +3,37 @@ package com.erikriosetiawan.moviecatalogue.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.erikriosetiawan.moviecatalogue.R
 import com.erikriosetiawan.moviecatalogue.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        loadFragment(MovieFragment())
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            var fragment: Fragment? = null
+
+            when (it.itemId) {
+                R.id.item_movie -> fragment = MovieFragment()
+                R.id.item_tv_show -> fragment = TvShowFragment()
+            }
+            return@setOnNavigationItemSelectedListener loadFragment(fragment)
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(binding.frameLayout.id, fragment)
+                .commit()
+            return true
+        }
+        return false
     }
 }

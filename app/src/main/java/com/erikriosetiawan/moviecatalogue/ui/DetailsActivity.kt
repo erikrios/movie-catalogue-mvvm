b6db1b16d63faca0ea2b5acac295abf3c3ae097a
erikrios <erikriosetiawan49@gmail.com>
2020-01-03 20:17:45 +0700

    Add variabel declaration

--- app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/DetailsActivity.kt
+++ app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/DetailsActivity.kt
@@ -5,14 +5,26 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.moviecatalogue.R
 import com.erikriosetiawan.moviecatalogue.databinding.ActivityDetailsBinding
+import com.erikriosetiawan.moviecatalogue.models.Movie
+import com.erikriosetiawan.moviecatalogue.models.TvShow
 
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
-    private lateinit var
+    private lateinit var movie: Movie
+    private lateinit var tvShow: TvShow
+    private lateinit var dataKey: String
+
+    companion object {
+        const val DATA_KEY = "data_key"
+        const val MOVIE_KEY = "movie_key"
+        const val TV_SHOW_KEY = "tv_show_key"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_details)
     }
+
+
 }

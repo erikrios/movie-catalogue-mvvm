c47c9ae49c79e11d8f0330b2a6f40e45685f3b9f
erikrios <erikriosetiawan49@gmail.com>
2020-01-03 18:50:00 +0700

    Add tv show response model

---
+++ app/src/main/java/com/erikriosetiawan/moviecatalogue/models/TvShowResponse.kt
@@ -0,0 +1,18 @@
+package com.erikriosetiawan.moviecatalogue.models
+
+import com.google.gson.annotations.SerializedName
+
+data class TvShowResponse(
+
+    @SerializedName("page")
+    var page: Int,
+
+    @SerializedName("total_results")
+    var totalResults: Int,
+
+    @SerializedName("total_pages")
+    var totalPages: Int,
+
+    @SerializedName("results")
+    var results: Int
+)

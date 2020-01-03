fad810f567ea11fe33e7060ca2537d55b3250169
erikrios <erikriosetiawan49@gmail.com>
2020-01-03 20:14:39 +0700

    Add details activity

--- app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/DetailsActivity.kt
+++ app/src/main/java/com/erikriosetiawan/moviecatalogue/ui/DetailsActivity.kt
@@ -2,12 +2,17 @@ package com.erikriosetiawan.moviecatalogue.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.moviecatalogue.R
+import com.erikriosetiawan.moviecatalogue.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailsBinding
+    private lateinit var
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_details)
     }
 }
